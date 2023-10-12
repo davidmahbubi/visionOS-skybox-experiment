0056d7dea2904494f9a39f305f1adddc2e79eb9d
David Mahbubi <ulrichdavid0370@gmail.com>
2023-10-12 13:45:15 +0700

    wip: Working on rotation

--- Skybox Test/ImmersiveView.swift
+++ Skybox Test/ImmersiveView.swift
@@ -11,17 +11,42 @@ import RealityKitContent
 import Combine
 
 struct ImmersiveView: View {
+    
+    var beachEntity: Entity
+    var lakeEntity: Entity
+    
+    @State var rotation: Double = 0
+    
+    init() {
+//        self.rotation = 55
+        
+        beachEntity = Entity()
+        beachEntity.addSkybox(for: "beach_scene", radius: 20)
+        beachEntity.updateRotation(rotation: 55)
+        
+        lakeEntity = Entity()
+        lakeEntity.addSkybox(for: "lake_scene", radius: 30)
+        lakeEntity.updateRotation(rotation: 55)
+    }
+    
     var body: some View {
         ZStack {
             RealityView { content in
                 let rootEntity = Entity()
-                rootEntity.addSkybox(for: "beach_scene", radius: 1e3)
+                rootEntity.addSkybox(for: "beach_scene", radius: 20)
+                rootEntity.updateRotation(rotation: 55)
                 content.add(rootEntity)
             }
             RealityView { content in
                 let rootEntity = Entity()
-                rootEntity.addSkybox(for: "lake_scene", radius: 1e2)
+                rootEntity.addSkybox(for: "lake_scene", radius: 10)
                 content.add(rootEntity)
+            }
+        }
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+//                rotation += 1000
+//                lakeEntity.updateRotation(rotation: rotation)
             }
         }
     }
@@ -47,7 +72,7 @@ extension Entity {
                     materials: [material]
                 ))
                 self.scale *= .init(x: -1, y: 1, z: 1)
-                self.position.y = 5
+//                self.position.y = 5
                 self.transform.translation += SIMD3<Float>(0.0, 1.0, 0.0)
                 
                 // Rotate the sphere to show the best initial view of the space.
@@ -84,10 +109,12 @@ extension Entity {
         components.set(Entity.SubscriptionComponent(subscription: subscription))
     }
     
-    func updateRotation() {
+    func updateRotation(rotation: Double = 55) {
         // Rotate the immersive space around the Y-axis set the user's
         // initial view of the immersive scene.
-        let angle = Angle.degrees(55)
+        let angle = Angle.degrees(rotation)
+//        print("Updating to \(angle.radians)")
+//        let rotation = simd_quatf(angle: Float(angle.radians), axis: SIMD3<Float>(0, 1, 0))
         let rotation = simd_quatf(angle: Float(angle.radians), axis: SIMD3<Float>(0, 1, 0))
         self.transform.rotation = rotation
     }
@@ -103,7 +130,7 @@ extension Entity {
 }
 
 
-#Preview {
-    ImmersiveView()
-        .previewLayout(.sizeThatFits)
-}
+//#Preview {
+//    ImmersiveView()
+//        .previewLayout(.sizeThatFits)
+//}
